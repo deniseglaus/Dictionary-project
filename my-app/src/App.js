c3b106b3b1a3184d6a85680635d1416d8d1248d7
deniseglaus <denise.glaus@gmail.com>
2022-07-20 17:52:12 +0200

    added Search Engine

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <h1>Hello this will be a Dictionary</h1>
+      <div classname="container">
+        <header className="App-header">
+          <h1>Hello this will be a Dictionary</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Denise</small>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ my-app/src/Dictionary.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  const [keyword, setKeyword] = useState(null);
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword}`);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} />
+      </form>
+    </div>
+  );
+}
