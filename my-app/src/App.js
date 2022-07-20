d394ef969bcb776ea5063ddb4239bd4c43676108
deniseglaus <denise.glaus@gmail.com>
2022-07-20 13:18:25 +0200

    initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,10 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <h1>Hello this will be a Dictionary</h1>
+    </div>
+  );
+}
